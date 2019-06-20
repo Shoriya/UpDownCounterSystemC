eb6bd238b795734612164d6ae3dff407108d7b7e
Shoriya Chauhan <49990606+Shoriya@users.noreply.github.com>
2019-06-21 00:27:21 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,16 @@
 int sc_main(int argc, char * argv[]) {
 
 	sc_signal<int> sig;
-	Counter* udcounter = new Counter("udcounter");
 	Testbench* countertb = new Testbench("countertb");
+	Counter* udcounter = new Counter("udcounter");
 
-	sc_clock Clock("Clock", 10, SC_NS, 0.5, 10, SC_NS, false);
+
+	sc_clock Clock("Clock", 20, SC_NS, 0.5, 0, SC_NS, false);
 
 	sc_signal<bool> reset;
 	sc_signal<bool> count_en;
 	sc_signal<bool> ud_ctrl;
-	sc_signal<sc_bv<17> > count_out;
+    sc_signal<sc_bv<17> > count_out;
 	sc_signal<bool> ovf_intr;
 	sc_signal<bool> unf_intr;
 
@@ -32,7 +33,18 @@ int sc_main(int argc, char * argv[]) {
 	countertb->ovf_intr(ovf_intr);
 	countertb->unf_intr(unf_intr);
 
-	cout<< "Start simulation" <<endl;
-	sc_start(10, SC_SEC);
+	cout<< sc_time_stamp() <<"Creating VCD file:\n";
+	sc_trace_file* file = sc_create_vcd_trace_file("UpDownCounter");
+
+	sc_trace(file,Clock,"clock");
+	sc_trace(file,reset,"reset");
+	sc_trace(file,count_en,"count_en");
+	sc_trace(file,ud_ctrl,"ud_ctrl");
+	sc_trace(file,count_out,"count_out");
+	sc_trace(file,ovf_intr,"ovf_intr");
+	sc_trace(file,unf_intr,"unf_intr");
+
+	cout<< "Start Simulation" <<endl;
+	sc_start();
 	return 0;
 }
